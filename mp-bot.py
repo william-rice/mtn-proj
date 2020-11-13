4db8293f84d45ec910c850845ca5e11636edfc83
William Rice <warice96@gmail.com>
2020-11-13 14:50:38 -0500

    fix main recursive function

--- mp-bot.py
+++ mp-bot.py
@@ -1,12 +1,14 @@
 import re
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as cond
+from selenium.webdriver.common.by import By
 
 # Use Chrome as our choice of browser
 # Go make sure you have google chromedriver installed in user/local/bin
 # In the selenium docs, 7.21 WebElement is a useful section to read
 driver = webdriver.Chrome()
-driver.implicitly_wait(5) # Because some things take time to load
+driver.implicitly_wait(20) # Because some things take time to load
 
 # Open Mtn Proj website
 driver.get('https://www.mountainproject.com/')
@@ -34,55 +36,89 @@ def set_search_settings(
     diffMinrock = driver.find_element_by_id("diffMinrock")
     Select(diffMinrock).select_by_visible_text(minimum_difficulty)
 
-    all_options = diffMinrock.find_elements_by_tag_name("option")
-    for option in all_options:
-        if option.is_selected():
-            print("{} is selected".format(option.text))
-
     # Set maximum difficulty
     diffMaxrock = driver.find_element_by_id("diffMaxrock")
     Select(diffMaxrock).select_by_visible_text(maximum_difficulty)
 
     all_options = diffMaxrock.find_elements_by_tag_name("option")
-    for option in all_options:
-        if option.is_selected():
-            print("{} is selected".format(option.text))
+
 set_search_settings()
 
+def get_element_from_list(link_elements, link_text):
+    """
+    Helper function for download_routes().
 
-def download_routes(current_level=1, current_area=None):
-    # Click "Change" to change area (do this every time)
+    Pulls out a specific area elements from a list of links.
+
+    Returns current link (the one w/ link_text)
+    and the next link in the list. This is a list of two items.
+
+    OR
+
+    Returns current link (just one item) if it's last in the list.
+    """
+    current_link = False
+
+    for el in link_elements:
+        if current_link: # false until next if statement executes
+            next_link = el
+            return [current_link, next_link]
+        if el.get_attribute("data-area-title") == link_text:
+            current_link = el
+
+    if current_link:
+        return current_link # if it's the last in the list, return just one
+
+def download_routes(path=["International"]):
+    print(path)
+
+    # Click "Change" to open dialog window (do this every time)
     routeFinderForm = driver.find_element_by_id("routeFinderForm")
     routeFinderForm.find_element_by_link_text("Change").click()
-    print(current_level)
-    if current_level > 1:
-        current_area.click()
 
-    # Select the area
-    area_list_element = driver.find_element_by_id("area-list")
-    areas = area_list_element.find_elements_by_xpath(".//a") # state links at level=0
+    for area in path:
+        # Get a list of the links in the window
+        links_container = driver.find_element_by_id("area-list")
+        links = links_container.find_elements_by_xpath(".//a")
 
-    for area in areas:
+        # Get the link for this area and the next (on this level)
+        [current_link, next_sibling] = get_element_from_list(links,area) # defined above
+        next_sibling_text = next_sibling.get_attribute("data-area-title")
+        current_text = current_link.get_attribute("data-area-title")
+        #print(cond.element_to_be_clickable(By.LINK_TEXT("International")))
+        #WebDriverWait(driver,10).until(lambda x: x.find_element_by_link_text(current_text).element_to_be_clickable())
+        current_link.click()
+        if area != path[-1]:
+            print("we continued")
+            continue # go all the way down the path
 
-        # ignore it if it's a level above:
-        containing_div = area.find_element_by_xpath("..")
-        link_level = containing_div.get_attribute("class")
-        if int(re.search("\d",link_level).group()) != current_level:
-            continue
+        # Get the first child name for navigation
+        new_links = links_container.find_elements_by_xpath(".//a")
+        first_child_index = len(path)
+        first_child = new_links[first_child_index]
+        first_child_text = first_child.get_attribute("data-area-title")
 
-        #print(area.get_attribute("data-area-title"))
-        area.click()
+        # Submit the form
         submit_area = driver.find_element_by_id("select-area")
         submit_area.click()
         routeFinderForm.submit()
+
+        # Only download pages w/ results <1000
         results_summary = driver.find_element_by_class_name("float-md-left")
         too_many_results = re.search("1000", results_summary.text)
+
         if too_many_results:
+            print("path had too many results")
             # Go one level deeper
+            path.append(first_child_text)
             driver.back()
-            download_routes(current_level+1, area)
+            download_routes(path)
         else:
-            print(area)
+            print("downloaded path") # this means download for now
+            path.pop()
+            path.append(next_sibling_text)
+            driver.back()
+            download_routes(path)
 
 download_routes()
 driver.close()
